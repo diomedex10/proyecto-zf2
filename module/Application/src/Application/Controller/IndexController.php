070c66d795965c65fd48fa5bba40a18d7993278d
diomedex10 <diomedex10@gmail.com>
2013-02-28 00:51:50 -0500

    Primera modificacion controller

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -16,6 +16,6 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel(array('titulo'=>'Hola Mundo'));
+        return new ViewModel(array('titulo'=>'Primer Commit'));
     }
 }
